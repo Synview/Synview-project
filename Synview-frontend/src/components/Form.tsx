fb2f23e113e7d615545cf7b576862a51993a8751
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-06-24 14:24:50 -0700

    Small fix to form css

--- Synview-frontend/src/components/Form.tsx
+++ Synview-frontend/src/components/Form.tsx
@@ -4,28 +4,35 @@ export default function Form() {
     <div className="mr-44 mt-44 w-92 text-start ">
       <fieldset className="fieldset border-1 p-4 rounded-box border-stone-600 bg-stone-800">
         <legend className="fieldset-legend text-white ">Register</legend>
-        <label>Username</label>
-        <input
-          className="input bg-stone-700"
-          typeof="text"
-          placeholder="Name"
-          id="Username"
-        ></input>
-        <label>Email</label>
-        <input
-          className="input bg-stone-700"
-          typeof="text"
-          placeholder="Email"
-          id="email"
-        ></input>
-        <label>Password</label>
-        <input
-          className="input bg-stone-700"
-          typeof="text"
-          placeholder="Password"
-          id="password"
-        ></input>
-        <button type="button" className="btn w-fit">Submit </button>
+        <div className="flex flex-col justify-center">
+          <label>Username</label>
+          <input
+            className="input bg-stone-700 w-full"
+            typeof="text"
+            placeholder="Name"
+            id="Username"
+          ></input>
+          <label>Email</label>
+          <input
+            className="input bg-stone-700 w-full"
+            typeof="text"
+            placeholder="Email"
+            id="email"
+          ></input>
+          <label>Password</label>
+          <input
+            className="input bg-stone-700 w-full"
+            typeof="text"
+            placeholder="Password"
+            id="password"
+          ></input>
+          <div className="flex justify-between items-center">
+            <button type="button" className="btn w-fit mt-2">
+              Submit
+            </button>
+            <a>Or Log in</a>
+          </div>
+        </div>
       </fieldset>
     </div>
   );
