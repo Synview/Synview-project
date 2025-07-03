a3c955f85a321a81fa3788b5bdb66c0a78606aff
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-03 10:12:26 -0700

    Erased unused function in JWT

--- Synview-backend/utils/JWTHelpers.ts
+++ Synview-backend/utils/JWTHelpers.ts
@@ -1,19 +1,5 @@
 import { Context } from "../deps.ts";
-export default async function Token(
-  context: Context,
-  next: () => Promise<unknown>
-) {
-  try {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      return;
-    }
-    context.response.headers.set("Authorization", `Bearer ${token}`)
-    await next();
-  } catch (e) {
-    throw new Error("Didnt find any auth token: " + e);
-  }
-}
+
 export async function generateKey() {
   return await crypto.subtle.generateKey(
     { name: "HMAC", hash: "SHA-512" },
