6c0ad6e4ecbd60fa311aa8c935c5bd5057bb0d2f
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-22 14:34:08 -0700

    Test 4, it Works!, now im thinking of how to call the other api with the data

--- Synview-backend/routes/webhook_route.ts
+++ Synview-backend/routes/webhook_route.ts
@@ -21,7 +21,8 @@ const prisma = new PrismaClient({
 webhookRouter.post("/github/webhook", async (context) => {
   try {
     const payload = context.request.body;
-    logger.info(await payload.json());
+    const text =  JSON.stringify(await payload.json());
+    logger.info(text);
     context.response.status = 200;
     context.response.body = {
       message: "Successfull webhook!",
