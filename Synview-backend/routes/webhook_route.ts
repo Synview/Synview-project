60a81fa074934df28e3e16edfaaa41f6f31e6b69
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-22 14:17:30 -0700

    Created the webhook route for github app to listen to

---
+++ Synview-backend/routes/webhook_route.ts
@@ -0,0 +1,24 @@
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { createLogger, LogLevel, rootLogger } from "../../common/Logger.ts";
+import { PrismaClient } from "../generated/prisma/client.ts";
+import { Router } from "@oak/oak";
+import { Session } from "https://deno.land/x/oak_sessions/mod.ts";
+
+const logger = createLogger("AI [API]", LogLevel.INFO);
+type AppState = {
+  session: Session;
+};
+
+const aiRouter = new Router<AppState>();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: Deno.env.get("DATABASE_URL")!,
+    },
+  },
+}).$extends(withAccelerate());
+
+aiRouter.post("/github/webhook", (context) => {
+  const payload = context.request.body;
+  logger.info(`Recieved : ${payload}`);
+});
