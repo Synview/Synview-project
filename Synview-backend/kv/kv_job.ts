037e26d74a865d07a44f49321c2c04dff80ae07a
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-24 02:58:14 -0700

    Deno kv job function to process long requests

---
+++ Synview-backend/kv/kv_job.ts
@@ -0,0 +1,59 @@
+import { createLogger } from "../../common/Logger.ts";
+import { recentCodeAnalysis } from "../AI/geminiHandler.ts";
+
+// @ts-check
+const kv = await Deno.openKv();
+const logger = createLogger("KV_JOB");
+
+type jobMessage = {
+  type: string;
+  aiJobId: string;
+  project_id: number;
+  commits: string[];
+  project_repo_url: string;
+  project_git_name: string;
+};
+
+kv.listenQueue(async (msg: jobMessage) => {
+  logger.info(`Processing analysis job`);
+  if (msg.type !== "projectAnalysis") {
+    logger.warn("Incorrect type of message");
+    return;
+  }
+
+  const { aiJobId, project_id, commits, project_repo_url, project_git_name } =
+    msg;
+  try {
+    logger.info("Started code analisis");
+    const response = await recentCodeAnalysis(
+      project_git_name,
+      project_repo_url,
+      commits.join(" ")
+    );
+
+    logger.info(`finished code analisis with response : ${response}`);
+    await kv.set(
+      ["jobs", aiJobId],
+      {
+        status: "complete",
+        response: response,
+        project_id: project_id,
+      },
+      { expireIn: 1000 * 60 * 60 * 24 }
+    );
+    logger.info("finished code analisis");
+  } catch (error) {
+    logger.error(
+      `Couldnt process code analysis in KV job with error : ${error}`
+    );
+    await kv.set(
+      ["jobs", aiJobId],
+      {
+        status: "failed",
+        response: error,
+        project_id: 0,
+      },
+      { expireIn: 1000 * 60 * 60 * 24 }
+    );
+  }
+});
