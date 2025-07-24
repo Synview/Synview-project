74ca018bf24727de5780691ef69a2bfface3ba0f
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-24 10:19:53 -0700

    wrapping kv listener on try catch

--- Synview-backend/kv/kv_job.ts
+++ Synview-backend/kv/kv_job.ts
@@ -14,47 +14,51 @@ type jobMessage = {
   project_git_name: string;
 };
 
-kv.listenQueue(async (msg: jobMessage) => {
-  logger.info(`Processing analysis job`);
-  if (msg.type !== "projectAnalysis") {
-    logger.warn("Incorrect type of message");
-    return;
-  }
+try {
+  kv.listenQueue(async (msg: jobMessage) => {
+    logger.info(`Processing analysis job`);
+    if (msg.type !== "projectAnalysis") {
+      logger.warn("Incorrect type of message");
+      return;
+    }
 
-  const { aiJobId, project_id, commits, project_repo_url, project_git_name } =
-    msg;
-    logger.info(`Job : ${aiJobId} started!`)
-  try {
-    logger.info("Started code analisis");
-    const response = await recentCodeAnalysis(
-      project_git_name,
-      project_repo_url,
-      commits.join(" ")
-    );
+    const { aiJobId, project_id, commits, project_repo_url, project_git_name } =
+      msg;
+    logger.info(`Job : ${aiJobId} started!`);
+    try {
+      logger.info("Started code analisis");
+      const response = await recentCodeAnalysis(
+        project_git_name,
+        project_repo_url,
+        commits.join(" ")
+      );
 
-    logger.info(`finished code analisis with response : ${response}`);
-    await kv.set(
-      ["jobs", aiJobId],
-      {
-        status: "complete",
-        response: response,
-        project_id: project_id,
-      },
-      { expireIn: 1000 * 60 * 60 * 24 }
-    );
-    logger.info("finished code analisis");
-  } catch (error) {
-    logger.error(
-      `Couldnt process code analysis in KV job with error : ${error}`
-    );
-    await kv.set(
-      ["jobs", aiJobId],
-      {
-        status: "failed",
-        response: error,
-        project_id: 0,
-      },
-      { expireIn: 1000 * 60 * 60 * 24 }
-    );
-  }
-});
+      logger.info(`finished code analisis with response : ${response}`);
+      await kv.set(
+        ["jobs", aiJobId],
+        {
+          status: "complete",
+          response: response,
+          project_id: project_id,
+        },
+        { expireIn: 1000 * 60 * 60 * 24 }
+      );
+      logger.info("finished code analisis");
+    } catch (error) {
+      logger.error(
+        `Couldnt process code analysis in KV job with error : ${error}`
+      );
+      await kv.set(
+        ["jobs", aiJobId],
+        {
+          status: "failed",
+          response: error,
+          project_id: 0,
+        },
+        { expireIn: 1000 * 60 * 60 * 24 }
+      );
+    }
+  });
+} catch (error) {
+    logger.error(`KV job couldnt recieve message with error: ${error}`)
+}
