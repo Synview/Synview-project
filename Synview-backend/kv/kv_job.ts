10865d436d02a1325bb9ab8940a8c7c0acb12634
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-24 11:00:13 -0700

    Removed try catch

--- Synview-backend/kv/kv_job.ts
+++ Synview-backend/kv/kv_job.ts
@@ -1,7 +1,5 @@
 import { createLogger } from "../../common/Logger.ts";
 import { recentCodeAnalysis } from "../AI/geminiHandler.ts";
-
-// @ts-check
 const kv = await Deno.openKv();
 const logger = createLogger("KV_JOB");
 
@@ -13,57 +11,48 @@ type jobMessage = {
   project_repo_url: string;
   project_git_name: string;
 };
+kv.listenQueue(async (msg: jobMessage) => {
+  logger.info(`Processing analysis job`);
+  const { aiJobId, project_id, commits, project_repo_url, project_git_name } =
+    msg;
 
-try {
-  kv.listenQueue(async (msg: jobMessage) => {
-    logger.info(`Processing analysis job`);
-
-    try {
-      if (msg.type !== "projectAnalysis") {
-        logger.warn("Incorrect type of message");
-        return;
-      }
-      const {
-        aiJobId,
-        project_id,
-        commits,
-        project_repo_url,
-        project_git_name,
-      } = msg;
-      logger.info(`Job : ${aiJobId} started!`);
-      logger.info("Started code analisis");
-      const response = await recentCodeAnalysis(
-        project_git_name,
-        project_repo_url,
-        commits.join(" ")
-      );
-
-      logger.info(`finished code analisis with response : ${response}`);
-      await kv.set(
-        ["jobs", aiJobId],
-        {
-          status: "complete",
-          response: response,
-          project_id: project_id,
-        },
-        { expireIn: 1000 * 60 * 60 * 24 }
-      );
-      logger.info("finished code analisis");
-    } catch (error) {
-      logger.error(
-        `Couldnt process code analysis in KV job with error : ${error}`
-      );
-      await kv.set(
-        ["jobs", aiJobId],
-        {
-          status: "failed",
-          response: error,
-          project_id: 0,
-        },
-        { expireIn: 1000 * 60 * 60 * 24 }
-      );
+  try {
+    if (msg.type !== "projectAnalysis") {
+      logger.warn("Incorrect type of message");
+      return;
     }
-  });
-} catch (error) {
-  logger.error(`KV job couldnt recieve message with error: ${error}`);
-}
+
+    logger.info(`Job : ${aiJobId} started!`);
+    logger.info("Started code analisis");
+    const response = await recentCodeAnalysis(
+      project_git_name,
+      project_repo_url,
+      commits.join(" ")
+    );
+
+    logger.info(`finished code analisis with response : ${response}`);
+    await kv.set(
+      ["jobs", aiJobId],
+      {
+        status: "complete",
+        response: response,
+        project_id: project_id,
+      },
+      { expireIn: 1000 * 60 * 60 * 24 }
+    );
+    logger.info("finished code analisis");
+  } catch (error) {
+    logger.error(
+      `Couldnt process code analysis in KV job with error : ${error}`
+    );
+    await kv.set(
+      ["jobs", aiJobId],
+      {
+        status: "failed",
+        response: error,
+        project_id: 0,
+      },
+      { expireIn: 1000 * 60 * 60 * 24 }
+    );
+  }
+});
