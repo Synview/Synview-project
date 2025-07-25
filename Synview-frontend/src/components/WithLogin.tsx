6c4562662f02b65705f3cb824934b55477276eed
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-25 15:14:32 -0700

    Slight better with login management

--- Synview-frontend/src/components/WithLogin.tsx
+++ Synview-frontend/src/components/WithLogin.tsx
@@ -1,10 +1,21 @@
 import { Navigate } from "react-router-dom";
+import { useGetPayloadQuery } from "../services/apiSlice.ts";
+import Loading from "./HelperComponents/Loading.tsx";
 type Props = {
   children: React.JSX.Element;
 };
 
 export default function WithLogin({ children }: Props) {
-  const hasAccess = !!localStorage.getItem("token");
+  const hasToken = !!localStorage.getItem("token");
+
+  const { data, error, isLoading } = useGetPayloadQuery(undefined, {
+    skip: !hasToken
+  });
+
+  const hasAccess = hasToken && !!data && !error;
+  if (isLoading) {
+    return <Loading />;
+  }
 
   if (!hasAccess) {
     return <Navigate to="/login" replace />;
