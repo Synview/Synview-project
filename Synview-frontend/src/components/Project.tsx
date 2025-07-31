c8a55c823989dce3e7a4c25e334cbe97149fc550
Juan Manuel Sandoval Ramírez <dusuan@meta.com>
2025-07-31 10:51:09 -0700

    Added extra info into the project cards

--- Synview-frontend/src/components/Project.tsx
+++ Synview-frontend/src/components/Project.tsx
@@ -1,19 +1,65 @@
- 
 import type { Project } from "../../../common/types.ts";
 import { Link } from "react-router-dom";
+import {
+  useGetMentorsQuery,
+  useGetMyUpdatesQuery,
+} from "../services/apiSlice.ts";
+import { skipToken } from "@reduxjs/toolkit/query";
+import { Badge } from "@mantine/core";
+import { Spoiler } from "@mantine/core";
 
 export default function Project({ title, description, project_id }: Project) {
+  const { data: projectUpdates, isLoading: isUpdatesLoading } =
+    useGetMyUpdatesQuery(String(project_id) ?? skipToken);
+  const { data: reviewingUsers, isLoading: isUsersLoading } =
+    useGetMentorsQuery(project_id ?? skipToken);
+
+  if (isUpdatesLoading || isUsersLoading) {
+    return;
+  }
+
+  const sortedUpdates = [...(projectUpdates ?? [])].sort((a, b) => {
+    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+  });
+
   return (
     <Link to={`/dashboard/project/${project_id}`}>
       <div className="card bg-neutral-800 shadow-sm h-[310px]">
-        <figure>
-          <img src="https://picsum.photos/300/150" alt="project"></img>
-        </figure>
-        <div className="card-body text-left">
-          <h2 className="card-title">{title}</h2>
-          <p>{description}</p>
-          <p>{}</p>
-          <div className="card-actions justify-end">
+        <div className="card-body text-left justify-between">
+          <div>
+            <div className="border-b border-neutral-600 flex flex-row items-center gap-4 pb-4">
+              <h2 className="card-title">{title}</h2>{" "}
+              <Badge color="rgba(96, 98, 143, 1)">
+                {projectUpdates?.length} Updates
+              </Badge>
+            </div>
+            <div className="pt-4">
+              <p className=" whitespace-normal  break-all">
+                Project description <br></br>{" "}
+                <Spoiler maxHeight={100} showLabel="..." hideLabel="Hide">
+                  {description}{" "}
+                </Spoiler>
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              Last update :
+              {sortedUpdates.length > 0
+                ? new Date(
+                    sortedUpdates.at(0)?.created_at!
+                  ).toLocaleDateString()
+                : " No updates yet"}
+            </div>
+            <div>
+              <Badge size="sm" color="gray">
+                {reviewingUsers?.length}
+                {reviewingUsers && reviewingUsers?.length === 0
+                  ? " Reviewers"
+                  : " Reviewer"}
+              </Badge>
+              <div className="card-actions justify-end"></div>
+            </div>
           </div>
         </div>
       </div>
